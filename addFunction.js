f8f012f346bf2b6ee49591c13113ca802ba348ee
Magdalena Kuna <madziakunia@gmail.com>
2020-07-29 20:59:24 +0200

    cleaning and little fixes

--- addFunction.js
+++ addFunction.js
@@ -2,12 +2,13 @@ function addElements(firstElement, secondElement) {
   let summation;
   const re = new RegExp('^\\s*$');
 
-  if((!firstElement || re.test(firstElement)) && (!secondElement ||
-  re.test(secondElement))){
+  if ((!firstElement || re.test(firstElement)) && 
+    (!secondElement || re.test(secondElement)))
+  {
     throw new Error('Nothing to do. Empty all elements.');
-  } else if (!firstElement || re.test(firstElement)){
+  } else if (!firstElement || re.test(firstElement)) {
     summation = secondElement;
-  } else if (!secondElement || re.test(secondElement)){
+  } else if (!secondElement || re.test(secondElement)) {
     summation = firstElement;
   } else {
     summation = firstElement + ' + ' + secondElement;
@@ -17,54 +18,59 @@ function addElements(firstElement, secondElement) {
 }
 
 function parsingSummation(summation) {
-  summation = summation.replace(/\s/g,'');
-  summation = summation.replace(/(?<=.+)(?<!\^)-/g,'+-');
+  summation = summation.replace(/\s/g, '');
+  summation = summation.replace(/(?<=.+)(?<!\^)-/g, '+-');
 
   const arrayElements = summation.split('+').filter(e => !!e);
 
   return arrayElements;
 }
 
-function arrayToObject(arrayElements){
+function arrayToObject(arrayElements) {
     let objectElements = {};
 
-    for (item of arrayElements) {
+    for (let item of arrayElements) {
 
-      if(item.includes('x')){
+      if (item.includes('x')) {
         item = item.replace(/x/g, '1');
         
         let phrases = item.split('^');
         
-        for (const prop in objectElements){
-          if(prop === phrases[1]){
+        for (const prop in objectElements) {
+          if (prop === phrases[1]) {
             phrases[0] = parseFloat(objectElements[prop]) +
             parseFloat(phrases[0]);
           }
         }
-        objectElements = {...objectElements, [phrases[1]]:
-          parseFloat(phrases[0])}
+        objectElements = {
+          ...objectElements,
+          [phrases[1]]: parseFloat(phrases[0])
+        };
       } else {
-        objectElements = {...objectElements, 0: parseFloat(item)}
+        objectElements = { 
+          ...objectElements, 
+          0: parseFloat(item)
+        }
       }
     }
-    
+
     return objectElements; 
 }
 
-function result(objectElements){
+function result(objectElements) {
     let finalResult = '';
 
-    for (const prop in objectElements){
-      if(prop === '0'){
+    for (const prop in objectElements) {
+      if (prop === '0'){
         finalResult += objectElements[prop];
       } else if (prop === '1') {
         finalResult = objectElements[prop] + 'x' + ' + ' +
         finalResult;
-      } else if (objectElements[prop] === 0){
+      } else if (objectElements[prop] === 0) {
         null;
-      } else if (objectElements[prop] === 1){
+      } else if (objectElements[prop] === 1) {
         finalResult = 'x^' + prop + ' + ' + finalResult;
-      } else if (objectElements[prop] === -1){
+      } else if (objectElements[prop] === -1) {
         finalResult = '-x^' + prop + ' + ' + finalResult;
       } else {
         finalResult = objectElements[prop] + 'x^' + prop +
@@ -72,38 +78,29 @@ function result(objectElements){
       }
     }
 
-    finalResult = finalResult.replace(/\s*\+\s*$/g,'');
-    finalResult = finalResult.replace(/\+\s*-/g,'-');
+    finalResult = finalResult.replace(/\s*\+\s*$/g, '');
+    finalResult = finalResult.replace(/\+\s*-/g, '-');
 
-    return finalResult
+    return finalResult;
 }
 
-function main(){
-  let x; //unknown
-  let firstElement = '2*x^2 + 3';
-  let secondElement = '3*x^3 + x^2';
-
+function main(firstElement, secondElement) {
   const summation = addElements(firstElement, secondElement);
   const arrayElements = parsingSummation(summation);
-  const objectElements =arrayToObject(arrayElements);
+  const objectElements = arrayToObject(arrayElements);
   const finalResult = result(objectElements);
 
   return finalResult;
 }
 
-console.log(main());
+// Change these arguments to try another statement.
+console.log(main('2*x^2 + 3', '3*x^3 + x^2'));
 
-(function() {
-  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-  module.exports = {
-    addElements,
-    parsingSummation,
-    arrayToObject,
-    result,
-    main,
-  };
-  } else {
-    window.main = main();
-    console.log(main);
-  }
-})();
+
+module.exports = {
+  addElements,
+  parsingSummation,
+  arrayToObject,
+  result,
+  main,
+};
--- addFunction.test.js
+++ addFunction.test.js
@@ -1,4 +1,4 @@
-const { add, addElements, parsingSummation, arrayToObject, result, main } = require('./addFunction.js');
+const { addElements, parsingSummation, arrayToObject, result, main } = require('./addFunction.js');
 
 const testObjects = {
     test1: {
@@ -50,58 +50,74 @@ const testObjects = {
 
 describe('Add function element', () => {
     test('it should add two integer elements, recruitment example', () => {
-        expect(addElements(testObjects.test1.elem1, testObjects.test1.elem2)).toBe(testObjects.test1.expectedAddResult)
+        expect(addElements(testObjects.test1.elem1, testObjects.test1.elem2))
+            .toBe(testObjects.test1.expectedAddResult);
     });
     test('it should add two float elements', () => {
-        expect(addElements(testObjects.test2.elem1, testObjects.test2.elem2)).toBe(testObjects.test2.expectedAddResult)
+        expect(addElements(testObjects.test2.elem1, testObjects.test2.elem2))
+            .toBe(testObjects.test2.expectedAddResult);
     });
     test('it should add two negative elements', () => {
-        expect(addElements(testObjects.test3.elem1, testObjects.test3.elem2)).toBe(testObjects.test3.expectedAddResult)
+        expect(addElements(testObjects.test3.elem1, testObjects.test3.elem2))
+            .toBe(testObjects.test3.expectedAddResult);
     });
     test('it should add two elements, one empty', () => {
-        expect(addElements(testObjects.test4.elem1, testObjects.test4.elem2)).toBe(testObjects.test4.expectedAddResult)
+        expect(addElements(testObjects.test4.elem1, testObjects.test4.elem2))
+            .toBe(testObjects.test4.expectedAddResult);
     });
     test('it should add two elements, one empty', () => {
-        expect(addElements(testObjects.test5.elem1, testObjects.test5.elem2)).toBe(testObjects.test5.expectedAddResult)
+        expect(addElements(testObjects.test5.elem1, testObjects.test5.elem2))
+            .toBe(testObjects.test5.expectedAddResult);
     });
     test('it should add two empty elements', () => {
-        expect(addElements.bind(this, testObjects.test6.elem1, testObjects.test6.elem2)).toThrow(new Error(testObjects.test6.expectedAddResult));
+        expect(addElements.bind(this, testObjects.test6.elem1, testObjects.test6.elem2))
+            .toThrow(new Error(testObjects.test6.expectedAddResult));
     });
 });
 
 describe('Parsing summation tests', () => {
     test('parsing two integer elements, recruitment example', () => {
-        expect(parsingSummation(testObjects.test1.expectedAddResult)).toEqual(expect.arrayContaining(testObjects.test1.expectedArray))
+        expect(parsingSummation(testObjects.test1.expectedAddResult))
+            .toEqual(expect.arrayContaining(testObjects.test1.expectedArray));
     });
     test('parsing two float elements', () => {
-        expect(parsingSummation(testObjects.test2.expectedAddResult)).toEqual(expect.arrayContaining(testObjects.test2.expectedArray))
+        expect(parsingSummation(testObjects.test2.expectedAddResult))
+            .toEqual(expect.arrayContaining(testObjects.test2.expectedArray));
     });
     test('parsing two negative elements', () => {
-        expect(parsingSummation(testObjects.test3.expectedAddResult)).toEqual(expect.arrayContaining(testObjects.test3.expectedArray))
+        expect(parsingSummation(testObjects.test3.expectedAddResult))
+            .toEqual(expect.arrayContaining(testObjects.test3.expectedArray));
     });
     test('parsing two elements, one emptys', () => {
-        expect(parsingSummation(testObjects.test4.expectedAddResult)).toEqual(expect.arrayContaining(testObjects.test4.expectedArray))
+        expect(parsingSummation(testObjects.test4.expectedAddResult))
+            .toEqual(expect.arrayContaining(testObjects.test4.expectedArray));
     });
     test('parsing two elements, one empty', () => {
-        expect(parsingSummation(testObjects.test5.expectedAddResult)).toEqual(expect.arrayContaining(testObjects.test5.expectedArray))
+        expect(parsingSummation(testObjects.test5.expectedAddResult))
+            .toEqual(expect.arrayContaining(testObjects.test5.expectedArray));
     });
 });
 
 describe('Parsing array to object', () => {
     test('parsing array to object two integer elements, recruitment example', () => {
-        expect(arrayToObject(testObjects.test1.expectedArray)).toEqual(expect.objectContaining(testObjects.test1.expectedObject))
+        expect(arrayToObject(testObjects.test1.expectedArray))
+            .toEqual(expect.objectContaining(testObjects.test1.expectedObject));
     });
     test('parsing array to object two float elements', () => {
-        expect(arrayToObject(testObjects.test2.expectedArray)).toEqual(expect.objectContaining(testObjects.test2.expectedObject))
+        expect(arrayToObject(testObjects.test2.expectedArray))
+            .toEqual(expect.objectContaining(testObjects.test2.expectedObject));
     });
     test('parsing array to object two negative elements', () => {
-        expect(arrayToObject(testObjects.test3.expectedArray)).toEqual(expect.objectContaining(testObjects.test3.expectedObject))
+        expect(arrayToObject(testObjects.test3.expectedArray))
+            .toEqual(expect.objectContaining(testObjects.test3.expectedObject));
     });
     test('parsing array to object two elements, one emptys', () => {
-        expect(arrayToObject(testObjects.test4.expectedArray)).toEqual(expect.objectContaining(testObjects.test4.expectedObject))
+        expect(arrayToObject(testObjects.test4.expectedArray))
+            .toEqual(expect.objectContaining(testObjects.test4.expectedObject));
     });
     test('parsing array to object two elements, one empty', () => {
-        expect(arrayToObject(testObjects.test5.expectedArray)).toEqual(expect.objectContaining(testObjects.test5.expectedObject))
+        expect(arrayToObject(testObjects.test5.expectedArray))
+            .toEqual(expect.objectContaining(testObjects.test5.expectedObject));
     });
 });
 
@@ -125,8 +141,11 @@ describe('Final statement', () => {
 
 describe('Main function', () => {
     test('final adding two integer elements, recruitment example', () => {
-        expect(main(result(arrayToObject(parsingSummation(addElements(testObjects.test1.elem1, testObjects.test1.elem2))))))
-        .toBe(testObjects.test1.finalStatement);
+        expect(main(testObjects.test1.elem1, testObjects.test1.elem2))
+            .toBe(testObjects.test1.finalStatement);
     });
-
+    test('final adding two negative elements', () => {
+        expect(main(testObjects.test3.elem1, testObjects.test3.elem2))
+            .toBe(testObjects.test3.finalStatement);
+    });
 });
